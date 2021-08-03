7933bbe2c2b0b42d455a9490dd1bfa0d9137cd89
joe-pool <88359905+joe-pool@users.noreply.github.com>
2021-08-03 14:51:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -374,7 +374,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/eskal/yiimp.git -b poolmine
+    git clone https://github.com/joe-pool/yiimp -b poolmine
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     make -j$((`nproc`+1))
